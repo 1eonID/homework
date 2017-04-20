208c8becf93e35b9ff332bb587fbccf0e4bba596
1eonID <lpogojev@gmail.com>
2017-04-20 19:09:01 +0300

    Create SimpleMain.java file

--- lpogojev/src/SimpleMain.java
+++
@@ -1,6 +0,0 @@
-public class SimpleMain {
-  public static void main(String[] args) {
-    // Here will be cool artifitial intelligence algorithm :)
-    System.out.println("Hello again... worm!");
-  }
-}
