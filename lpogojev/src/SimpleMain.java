1bff79d5d7586dc3ef4c0ae32a4224dcdf86700f
1eonID <lpogojev@gmail.com>
2017-04-20 19:10:44 +0300

    Edit SimpleMain.java file

---
+++ lpogojev/src/SimpleMain.java
@@ -0,0 +1,6 @@
+public class SimpleMain {
+  public static void main(String[] args) {
+    // Here will be cool artifitial intelligence algorithm :)
+    System.out.println("Hello again... worm!");
+  }
+}
