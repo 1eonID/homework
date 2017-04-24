2b94e5f1dcaf7b39053640b74b7695ad1ba90c99
1eonID <lpogojev@gmail.com>
2017-04-24 17:27:09 +0300

    Solved the first task

---
+++ lesson4/lpogojev/src/FirstTask.java
@@ -0,0 +1,12 @@
+public class FirstTask {
+	public static void main(String[] args) {
+		int a = 99;
+		float f = 33f;
+		String str = "Some string";
+
+		System.out.println(a);
+		System.out.println(f);
+		System.out.println(str);
+
+	}
+}
