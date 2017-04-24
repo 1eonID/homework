58cee4337a2a67d0e677432cab3cefa75344945c
1eonID <lpogojev@gmail.com>
2017-04-24 17:28:10 +0300

    Solved the second task

---
+++ lesson4/lpogojev/src/SecondTask.java
@@ -0,0 +1,22 @@
+public class SecondTask {
+	public static void main(String[] args) {
+
+		// starprint();
+		// starprint();
+		// starprint();
+		// starprint();
+		// starprint();
+		starprint();
+
+	}
+
+	// static void starprint() {
+	// 	System.out.println("*****");
+	// }
+
+	static void starprint() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("*****");
+		}
+	}
+}
