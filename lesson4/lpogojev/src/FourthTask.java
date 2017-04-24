154e8645843fad37466866772aca77292186ffe9
1eonID <lpogojev@gmail.com>
2017-04-24 17:33:04 +0300

    Solved the fourth task

---
+++ lesson4/lpogojev/src/FourthTask.java
@@ -0,0 +1,14 @@
+public class FourthTask {
+	public static void main(String[] args) {
+		int i = 0;
+		do {
+			if (i > 4) break;
+			infitecycle();
+			i++;
+		} while(true);
+	}
+
+	public static void infitecycle() {
+		System.out.println("It seems that I'm here forever");
+	}
+}
